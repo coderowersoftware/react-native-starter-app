8b857bd6c8658acb9b9145ea14a025bff3f56971
Garima Kaila <garimakaila03@gmail.com>
2017-05-28 19:35:53 +0200

    Added iOS app mode

--- index.ios.js
+++ index.ios.js
@@ -4,50 +4,152 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+    AppRegistry,
+    StyleSheet,
+    Text,
+    TextInput,
+    Image,
+    Button,
+    Animated,
+    ScrollView,
+    ToolbarAndroid,
+    View
 } from 'react-native';
 
-export default class StarterApp extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
+import {
+    StackNavigator,
+    TabNavigator,
+    DrawerNavigator
+} from 'react-navigation';
+var Container = require('./app/components/layout/Container');
+//var Button = require('./app/components/buttons/Button');
+var Footer = require('./app/components/layout/Footer');
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+var MoviesList = require('./app/views/movies/MoviesList');
+
+var GoogleSignInButton = require('./app/components/buttons/GoogleSignInButton');
+var FacebookSignInButton = require('./app/components/buttons/FacebookSignInButton');
+
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+import StarterApp from "./app/App"
+
+import Store from './app/store/Store';
+/*
+
+ var styles = StyleSheet.create({
+ toolbar: {
+ backgroundColor: '#9999ff',
+ height: 56,
+ },
+ scrollView: {
+ flex: 1,
+ backgroundColor: 'blue'
+ },
+ container: {
+ flex: 1,
+ paddingTop: 65,
+ backgroundColor: 'steelblue'
+
+ },
+ });
+
+
+ export default class StarterApp extends Component {
+ constructor(props) {
+ super(props);
+ this.state = {
+ userDetails: {}
+ };
+ }
+
+ componentDidMount() {
+ Store.subscribe(() => {
+ this.setState({
+ userDetails: Store.getState().login.userDetails
+ });
+
+ //   alert("hello " + JSON.stringify(Store.getState().login.userDetails, null, '  '));
+ });
+ }
+
+
+ render() {
+ return (
+ <View style={{flex: 1}}>
+ <ToolbarAndroid
+ title='Starter App'
+ style={styles.toolbar}
+ actions={[{title: (this.state.userDetails.id && this.state.userDetails.id.length > 0)?"Welcome "+this.state.userDetails.name + " (" + this.state.userDetails.email + ")":"", show: 'always'}]}
+ onActionSelected={this.onActionSelected}/>
+ <Container>
+ <GoogleSignInButton></GoogleSignInButton>
+ <FacebookSignInButton></FacebookSignInButton>
+ <MoviesList></MoviesList>
+ </Container>
+ <Footer></Footer>
+ </View>
+ );
+ }
+
+ }
+ */
+
+/*
+
+ class MyHomeScreen extends React.Component {
+ static navigationOptions = {
+ drawerLabel: 'Home',
+ };
+
+ render() {
+ return (
+ <Button
+ onPress={() => this.props.navigation.navigate('Notifications')}
+ title="Go to notifications"
+ />
+ );
+ }
+ }
+
+ class MyNotificationsScreen extends React.Component {
+ static navigationOptions = {
+ drawerLabel: 'Notifications',
+ drawerIcon: ({ tintColor }) => (
+ <Icon name="rocket" size={30} color="#900" />/!* <Image
+ source={require('./app/notif-icon.png')}
+ style={[styles.tabIcon, {tintColor: tintColor}]}
+ />*!/
+ ),
+ };
+
+ render() {
+ return (
+ <Button
+ onPress={() => this.props.navigation.goBack()}
+ title="Go back home"
+ />
+ );
+ }
+ }
+
+ const styles = StyleSheet.create({
+ icon: {
+ width: 24,
+ height: 24,
+ },
+ });
+
+ const StarterApp = DrawerNavigator({
+ Home: {
+ screen: MyHomeScreen,
+ },
+ Notifications: {
+ screen: MyNotificationsScreen,
+ },
+ });
+ */
 
 AppRegistry.registerComponent('StarterApp', () => StarterApp);
--- ios/StarterApp/AppDelegate.m
+++ ios/StarterApp/AppDelegate.m
@@ -11,6 +11,11 @@
 
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#import <RNGoogleSignin/RNGoogleSignin.h>
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+
+
 
 @implementation AppDelegate
 
@@ -31,7 +36,29 @@
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
-  return YES;
+
+  return [[FBSDKApplicationDelegate sharedInstance] application:application
+                                  didFinishLaunchingWithOptions:launchOptions];
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+  [FBSDKAppEvents activateApp];
+}
+
+
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url
+  sourceApplication:(NSString *)sourceApplication annotation:(id)annotation {
+  
+  return [[FBSDKApplicationDelegate sharedInstance] application:application
+                                                        openURL:url
+                                              sourceApplication:sourceApplication
+                                                     annotation:annotation
+          ]
+  || [RNGoogleSignin application:application
+                         openURL:url
+               sourceApplication:sourceApplication
+                      annotation:annotation
+      ];
 }
 
 @end
